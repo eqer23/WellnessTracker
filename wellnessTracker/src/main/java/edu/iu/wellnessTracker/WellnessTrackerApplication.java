0002726619828fd2f41fd6998ed683bb5d7cdf5f
Tim Chan <timchatan@gmail.com>
2024-02-03 12:09:33 -0500

    starter springboot for backend

---
+++ wellnessTracker/src/main/java/edu/iu/wellnessTracker/WellnessTrackerApplication.java
@@ -0,0 +1,13 @@
+package edu.iu.wellnessTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellnessTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellnessTrackerApplication.class, args);
+	}
+
+}
---
+++ wellnessTracker/src/test/java/edu/iu/wellnessTracker/WellnessTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.wellnessTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellnessTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
