d108fc3d59bf297acdcd8bf69ffda667e9f9b152
Tim Chan <timchan@iu.edu>
2024-02-10 14:17:04 -0500

    registration error code if exists

--- server/controllers/registration.js
+++ server/controllers/registration.js
@@ -4,16 +4,27 @@ const salt = 10;
 
 const registrationPostController = async (req, res) => {
   const { username, password, role } = req.body;
-  const hashPassword = await bcrypt.hash(password, salt);
 
-  const newUser = new User({
-    username: username,
-    password: hashPassword,
-    role: role
-  });
+  if (role === "user" || role === 'professional') {
+    const user = await User.findOne({ username });
+    if (user) {
+      res.status(409).json({ message: "Client is already registered." });
+      console.log('Account exists')
+      return;
+    }
+  }
+  else {
+    const hashPassword = await bcrypt.hash(password, salt);
 
-  await newUser.save();
-  console.log("Account created.");
+    const newUser = new User({
+      username: username,
+      password: hashPassword,
+      role: role
+    });
+
+    await newUser.save();
+    console.log("Account created.");
+  }
 };
 
 export default registrationPostController;
