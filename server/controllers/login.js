d15e019420196eb232580a0de4e36d38e767e04b
Tim Chan <timchan@iu.edu>
2024-02-10 12:24:45 -0500

    removed client for brevity and fixed pro login

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -4,10 +4,10 @@ import bcrypt from "bcrypt";
 // login function, post to login
 const loginPostController = async (req, res) => {
   const { username, password, role } = req.body;
-  if (role === "user") {
+  if (role === "user" || role === 'professional') {
     const user = await User.findOne({ username });
     if (!user) {
-      res.json({ message: "user not registered." });
+      res.json({ message: "client not registered." });
     }
     const validPassword = await bcrypt.compare(password, user.password);
 
@@ -20,7 +20,7 @@ const loginPostController = async (req, res) => {
     );
     res.cookie("token", token, { httpOnly: true, secure: true });
     return res.json({ login: true, role: "user" });
-  } else if (role === "client") {
+  } else if (role === "professional") {
   } else {
   }
 };
