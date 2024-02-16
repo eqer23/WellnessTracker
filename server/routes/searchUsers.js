57cff0a45beb42d19f66f590013abc376dc0113c
camillefraizer <camille.fraizer@gmail.com>
2024-02-16 14:53:52 -0500

    changes to searchUsers route

--- server/routes/searchUsers.js
+++ server/routes/searchUsers.js
@@ -2,3 +2,16 @@ import express from "express";
 const router = express.Router();
 import "../db.js";
 import searchContentPostController from "../controllers/searchPeople.js";
+
+const User = require("../models/User");
+
+router.get("/users", async(req, res) => {
+    try{
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: true, message: "Internal server error"});
+    }
+});
+
+export { router as searchUsersRouter };
