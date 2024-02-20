e9502416754ae77bf9b08bfa812e8c16bb4c8dc4
Isaac Navarrete <navarreteisaac14@gmail.com>
2024-02-20 18:06:21 -0500

    Added some uploading backend
    Next step is to get postman working and make a request to the backend based on uploadContent.js

---
+++ server/controllers/uploadContent.js
@@ -0,0 +1,27 @@
+const { Content } = require("../models/Content");
+const multer = require("multer");
+
+const store = (req, res, next) => {
+    let content = new Content({
+        contentTitle : req.body.name,
+        dateCreated : req.body.dateCreated,
+        contentType: req.body.contentType,
+        contentContents : req.body.content,
+        creatorID : req.body.creatorID
+    })
+    if(req.file){
+        content.contentContents = req.file.path
+    }
+
+    content.save()
+    .then(response => {
+        res.json({
+            message : "UPLOADED!!!!!"
+        })
+    })
+    .catch(error => {
+        res.json({
+            message: "An error Occured!"
+        })
+    })
+}
