19246b2c3bf2f10d16e678c28c958c162e5117ba
camillefraizer <camille.fraizer@gmail.com>
2024-02-19 21:10:54 -0500

    created Content.js and updated searchUsers

--- server/controllers/searchUsers.js
+++ server/controllers/searchUsers.js
@@ -2,7 +2,7 @@ import { models } from "mongoose";
 import {User} from "../models.User.js"
 
 
-const registrationPostController = async (req, res) => {
+const searchUsersPostController = async (req, res) => {
     const { email, firstName, lastName, role } = req.body;
 
     
