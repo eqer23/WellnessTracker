e405529e6d2600654821b805dc0f78cc0fb1b4ba
Tim Chan <timchan@iu.edu>
2024-02-26 20:40:42 -0500

    merge profile-dev

--- server/models/User.js
+++ server/models/User.js
@@ -1,48 +1,61 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    firstName : {
-        type: String,
-        // required: true
-    },
-    lastName : {
-        type: String,
-        // required: true
-    },
-    email : { // changed from `username` since the email will be the username
-        type: String, 
-        required: true, 
-        unique: true
-    },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+  },
+  email: {
+    // changed from `username` since the email will be the username
+    type: String,
+    required: true,
+    unique: true,
+  },
 
-    password : {
-        type: String, 
-        required: true
-    },
+  password: {
+    type: String,
+    required: true,
+  },
 
-    role : {
-        type: String, 
-        required: true
-    },
+  role: {
+    type: String,
+    required: true,
+  },
 
-    gender : {
-        type : String
-    }, 
+  phone: {
+    type: String,
+  },
 
-    city : {
-        type : String
-    }, 
+  gender: {
+    type: String,
+  },
 
-    state : {
-        type : String
-    },
+  city: {
+    type: String,
+  },
 
-    specialty : {
-        type : String
-    }    
-})
+  state: {
+    type: String,
+  },
 
-const userModel = mongoose.model('User',userSchema)
+  specialty: {
+    type: String,
+  },
+  // this is what you input into the authenticator
+  tfaToken: {
+    type: String,
+  },
+  // this is the id you send the 6 number code to
+  tfaTokenId: {
+    type: String,
+  },
+});
+
+const userModel = mongoose.model("User", userSchema);
+
 module.exports = {
-    User: userModel
+  User: userModel,
 };
