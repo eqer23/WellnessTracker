412b4afc997dd1a7d593ae44d6b19d57252676e1
Tim Chan <timchan@iu.edu>
2024-03-01 11:11:21 -0500

    cors

--- server/index.js
+++ server/index.js
@@ -46,19 +46,25 @@ main();
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://instafit-frontend.onrender.com'); 
-  res.setHeader('Access-Control-Allow-Origin', 'https://instafit-backend.onrender.com'); 
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://instafit-frontend.onrender.com'); 
+//   res.setHeader('Access-Control-Allow-Origin', 'https://instafit-backend.onrender.com'); 
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
+
+// app.use(cors({
+//   origin: ['https://instafit-frontend.onrender.com', 'https://another-frontend.onrender.com'],
+//   credentials: true,
+// }));
 
 app.use(cors({
-  origin: ['https://instafit-frontend.onrender.com', 'https://another-frontend.onrender.com'],
+  origin: ['https://instafit-frontend.onrender.com', 'https://instafit-backend.onrender.com'],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-
 
 app.use(cookieParser());
 dotenv.config();
