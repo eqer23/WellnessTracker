a5343fbeeae5320273b7df4873f58eadd1895b36
Tim Chan <timchan@iu.edu>
2024-03-05 16:40:53 -0500

    chat and get all users added

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -1,14 +1,16 @@
-const { User } = require("../models/User.js");
+// const { User } = require("../models/User.js");
 
-const getAllUsers = async (req, res, next) => {
-    try {
-        const users = await User.find({_id:{$ne:req.params.id}}).select([
-            "email",
-            "_id"
-        ]);
-        return res.json(users);
-    }
-    catch (err) {
-        console.log(err);
-    }
-}
+// const getAllUsers = async (req, res, next) => {
+//     try {
+//         const users = await User.find({_id:{$ne:req.params.id}}).select([
+//             "email",
+//             "_id"
+//         ]);
+//         return res.json(users);
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// }
+
+// module.exports = getAllUsers;
