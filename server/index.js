f8f7fe36882272a6d708a8aa3f92a820162189aa
sierranapieralski <111990838+sierranapieralski@users.noreply.github.com>
2024-03-23 17:28:59 -0400

    removed some comments

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { authRouter } = require("./routes/auth.js");
 
-
 // adding mongodb stuff from video:
 const { MongoClient } = require("mongodb");
 const { dataRouter } = require("./routes/data.js");
@@ -16,58 +15,30 @@ async function dbConnect() {
     let result = await client.connect();
     db = result.db(databaseName);
     return db.collection("users");
-    // let data = await collection.find({}).toArray();
-    // console.log(data);
 }
-
-// module.exports= dbConnect; // only if we put the mongo stuff above into a seprate file
-
-// console.log(dbConnection());
-
-// dbConnection().then((resp) => {
-//     resp.find() //if the fint () I can add {} and say username:'blah' to get all docunments with blah of username
-//         .toArray()
-//         .then((data) => {
-//             console.log(data);
-//         });
-// });
-
-// const dbConnect=require('./mongodb'); // only if above is in a different file
 
 // another way to do what we did above:
 const main = async () => {
     let data = await dbConnect();
     data = await data.find().toArray(); // can add {} to find () as above ^^^
-    console.log(data);
+    // console.log(data);
 };
 
 main();
 
 const app = express();
-// <<<<<<< UserLogin_TimDev
-app.use(express.json())
-app.use(cors({
-    origin: true,
-    credentials: true
-}))
-app.use(cookieParser())
-dotenv.config()
-app.use('/', authRouter)
-app.use('/', dataRouter)
 
-// =======
-// app.use(express.json());
-// app.use(
-//     cors({
-//         origin: true,
-//         credentials: true,
-//     })
-// );
-// app.use(cookieParser());
-// dotenv.config();
-// app.use("/", userLoginRouter);
-// app.use("/", userRegisterRouter);
-// >>>>>>> sierraBackEnd
+app.use(express.json());
+app.use(
+    cors({
+        origin: true,
+        credentials: true,
+    })
+);
+app.use(cookieParser());
+dotenv.config();
+app.use("/", authRouter);
+app.use("/", dataRouter);
 
 app.listen(process.env.PORT, () => {
     console.log("Server is running");
